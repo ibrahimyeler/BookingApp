216030da2918a0868ccd0cdb2460211e6ad1f596
ibrahimyeler <ibrahimyeler10@gmail.com>
2024-08-14 17:50:43 +0300

    user registration api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,31 +2,19 @@ import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
 
 // MongoDB bağlantısı
-const mongoConnectionString = process.env.MONGODB_CONNECTION_STRING as string;
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
-if (!mongoConnectionString) {
-  console.error('MONGO_CONNECTION_STRING environment variable is not defined');
-  process.exit(1);
-}
 
-mongoose.connect(mongoConnectionString)
-  .then(() => console.log('MongoDB connected successfully'))
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1);
-  });
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-// Test endpoint
-app.get("/api/test", async (req: Request, res: Response) => {
-   res.json({ message: "hello from express endpoint!" });
-});
+app.use("/api/users" ,userRoutes);
 
 // Sunucuyu başlat
 app.listen(7000, () => {
