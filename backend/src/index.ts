fa1ecd0fd08d0b1650cc19eeaab3676216124488
ibrahimyeler <ibrahimyeler10@gmail.com>
2024-08-15 00:32:48 +0300

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';
 import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
 
 // MongoDB bağlantısı
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -17,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes);
 
 // Sunucuyu başlat
