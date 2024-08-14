5074e2c621ead895d3f4c672bc0aac71b06080bc
ibrahimyeler <ibrahimyeler10@gmail.com>
2024-08-14 14:50:10 +0300

    Header error fixed

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,8 +1,10 @@
-const Layout = () => {
+import Header from "../components/Header.tsx";
 
+const Layout = () => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header />
         </div>
-    )
-}
+    );
+};
+export default Layout;
